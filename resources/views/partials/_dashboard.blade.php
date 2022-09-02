be1b5ca7a2bfa872623d5893e801c302a05ff696
Caesar127 <takondwacaesarndawala@gmail.com>
2022-09-02 08:46:19 +0200

    Dashboard

--- resources/views/partials/_dashboard.blade.php
+++ resources/views/partials/_dashboard.blade.php
@@ -14,7 +14,15 @@
   <div style="background: #fff; height: 500px; width: 400px; border-radius: 20px; margin:20px;">
   </div>
   <div class="grid-container">
-      <div class="grid-item">1</div>
+      <div class="grid-item" style="display: flex; justify-content: space-between;">
+        <div class="" style="padding-left: 0.2rem;">
+          <img src="images/icons8-faculty-100.png" class="w-min h-max" alt="">
+        </div>
+        <div class="" style="padding-right: 0.4rem;">
+          <p>8</p>
+          <p>Faculties</p>
+        </div>
+    </div>
       <div class="grid-item">2</div>
       <div class="grid-item">3</div>  
       <div class="grid-item">4</div>
