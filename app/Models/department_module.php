cadc5350e6cf454ed48f9f2028ffd8a43114d1ac
Caesar127 <takondwacaesarndawala@gmail.com>
2022-09-06 12:45:33 +0200

    Model

--- app/Models/department_module.php
+++ app/Models/department_module.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class department_module extends Model
 {
     use HasFactory;
+    protected $table = 'department_module';
+
+    protected $fillable = ['department','Year','semester','module'];
+
+    public function scopeFilter($query, array $filters) {
+        if($filters['search'] ?? false) {
+            $query->where('Year', 'like', '%' . request('search') . '%')
+                ->orWhere('semester', 'like', '%' . request('search') . '%');
+        }
+    } 
 }
