a51812d4eaeac09d5cce1191007c0dc7df21de67
Caesar127 <takondwacaesarndawala@gmail.com>
2022-09-06 15:39:46 +0200

    department

--- resources/views/department.blade.php
+++ resources/views/department.blade.php
@@ -102,7 +102,7 @@
                             </a>
                             <ul class=" dropdown-menu min-w-max absolute bg-white text-base z-50 float-left py-2 list-none text-left rounded-lg shadow-lg mt-1 hidden m-0 bg-clip-padding border-none" aria-labelledby="dropdownMenuButton2">                            
                               <li>
-                                <a class=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 " href="#">Update</a >
+                                <a class=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 " href="/department/{{$departments->department_code}}/edit">Update</a >
                               </li>
                               <li>
                                 <a class=" dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100 " href="#">Delete</a >
