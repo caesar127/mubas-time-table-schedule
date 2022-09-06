b1309ee5e2f101a0ee31b6148a89867ac3cb6b3f
Caesar127 <takondwacaesarndawala@gmail.com>
2022-09-06 13:42:46 +0200

    department

--- resources/views/department.blade.php
+++ resources/views/department.blade.php
@@ -27,11 +27,22 @@
               </div>
               <div class="form-group mb-6"><input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2" placeholder="Department Name">
               </div>
-              <div class="form-group mb-6">
-                <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2" placeholder="Faculty">
-              </div>
-              <div class="form-group mb-6">
-                <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInputEmail2" placeholder="No of Modules">
+              <div class="form-check mb-6">
+                <div class="flex justify-center">
+                  <div class="mb-3 xl:w-96">
+                    <select class="form-select appearance-none block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example" name="module">
+                        <option selected>Faculty</option>
+                        <?php $faculty =DB::table('faculties')->select('faculty_code', 'name') ->get();?>
+                        @unless (count($faculty) == 0)
+                          @foreach ($faculty as $faculty)
+                            <option value="{{$faculty->faculty_code}}">{{$faculty->name}}</option>
+                          @endforeach
+                        @else
+                          <option>No modules available</option>
+                        @endunless
+                    </select>
+                  </div>
+                </div>
               </div>
               <button type="submit" class=" w-full px-6 py-3.5 bg-blue-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-500 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" style="background: #0184CC">ADD</button>
             </form>
@@ -66,10 +77,12 @@
                       {{$departments->name}}
                     </td>
                     <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {{$departments->faculty}}
+                      <?php $faculty = DB::table('faculties')->where('faculty_code',$departments->faculty)->value('name');?>
+                        {{$faculty}}
                     </td>
                     <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                      {{$departments->modules}}
+                      <?php $modules = DB::table('department_module')->where('department',$departments->department_code)->get();?>
+                        {{count($modules)}}
                     </td>
                     <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                       <div class="flex justify-center">
