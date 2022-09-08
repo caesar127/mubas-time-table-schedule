fab99e8939ca111b0d46581a824fedf9fb7d0924
Caesar127 <takondwacaesarndawala@gmail.com>
2022-09-08 12:50:25 +0200

    Time Table

--- app/Http/Controllers/TimetableController.php
+++ app/Http/Controllers/TimetableController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use DateTime;
+use DateInterval;
+use App\Models\department_module;
+use App\Models\module;
 use Illuminate\Http\Request;
 
 class TimetableController extends Controller
@@ -34,7 +38,29 @@ class TimetableController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $input_date = $request->input('date');
+        $input_date = date('d/m/y', strtotime($input_date));
+        $input_date = strtotime($input_date);
+        $start_date = strtotime("+70 day", $input_date);
+        $end_date = strtotime("+80 day", $input_date);
+        
+        $modules = department_module::get();
+ 
+        $modules = $modules->reject(function ($module) {
+            return $module->cancelled;
+        });
+
+        foreach($modules as $module){
+            $exam_date= mt_rand($start_date,$end_date);
+
+            $day = date('D', $exam_date);
+            if ($day == 'Sun' || $day == 'Sat'){
+                $this->store($request);
+            }else{
+                
+            }
+            dd(date('D', $exam_date));
+        }
     }
 
     /**
