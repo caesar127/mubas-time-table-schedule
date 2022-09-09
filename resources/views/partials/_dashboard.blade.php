2597e45136af82ef3a83d9199ee040ed1fa7054e
Caesar127 <takondwacaesarndawala@gmail.com>
2022-09-09 19:56:53 +0200

    Dashboard

--- resources/views/partials/_dashboard.blade.php
+++ resources/views/partials/_dashboard.blade.php
@@ -9,12 +9,13 @@
     margin: 20px;
     font-size: 30px;
     width: 250px;
+    height: 115px;
     text-align: center;
   }
  </style>
  
  
-  <div style="background: #fff; height: 500px; width: 400px; border-radius: 20px; margin:20px;">
+  <div style="background: #fff; height: 440px; width: 350px; border-radius: 20px; margin:20px;">
     <h1>Today calendar (skin-black shadow-default)</h1>
     
     <div class="calendar-container">
@@ -24,54 +25,54 @@
   <div class="grid-container">
       <div class="grid-item" style="display: flex; justify-content: space-between;">
         <div class="" style="padding-left: 0.2rem; padding-top: 1.5rem;">
-          <img src="images/icons8-faculty-100.png" class="w-min h-max" alt="">
+          <img src="images/icons8-faculty-100.png" alt="" width="80px">
         </div>
-        <div class="" style="font-size: smaller; text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
+        <div class="" style="font-size: 20px; text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
           <p>8</p>
           <p>Faculties</p>
         </div>
       </div>
       <div class="grid-item" style="display: flex; justify-content: space-between;">
         <div class="" style="padding-left: 0.2rem; padding-top: 1.5rem;">
-          <img src="images/icons8-department-100.png" class="w-min h-max" alt="">
+          <img src="images/icons8-department-100.png" alt="" width="80px">
         </div>
-        <div class="" style=" text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
+        <div class="" style="font-size: 20px; text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
           <p>8</p>
           <p>Departm...</p>
         </div>
       </div>
       <div class="grid-item" style="display: flex; justify-content: space-between;">
         <div class="" style="padding-left: 0.2rem; padding-top: 1.5rem;">
-          <img src="images/icons8-training-100 (1).png" class="w-min h-max" alt="">
+          <img src="images/icons8-training-100 (1).png" alt="" width="80px">
         </div>
-        <div class="" style=" text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
+        <div class="" style="font-size: 20px; text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
           <p>8</p>
           <p>Lecturers</p>
         </div>
       </div>
       <div class="grid-item" style="display: flex; justify-content: space-between;">
         <div class="" style="padding-left: 0.2rem; padding-top: 1.5rem;">
-          <img src="images/module.png" class="w-min h-max" alt="">
+          <img src="images/module.png" alt="" width="80px">
         </div>
-        <div class="" style=" text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
+        <div class="" style="font-size: 20px; text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
           <p>8</p>
           <p>Modules</p>
         </div>
       </div>
       <div class="grid-item" style="display: flex; justify-content: space-between;">
         <div class="" style="padding-left: 0.2rem; padding-top: 1.5rem;">
-          <img src="images/icons8-class-100.png" class="w-min h-max" alt="">
+          <img src="images/icons8-class-100.png" alt="" width="80px">
         </div>
-        <div class="" style=" text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
+        <div class="" style="font-size: 20px; text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
           <p>8</p>
           <p>Classes</p>
         </div>
       </div>
       <div class="grid-item" style="display: flex; justify-content: space-between;">
         <div class="" style="padding-left: 0.2rem; padding-top: 1.5rem;">
-          <img src="images/icons8-room-100.png" class="w-min h-max" alt="">
+          <img src="images/icons8-room-100.png" alt="" width="80px">
         </div>
-        <div class="" style=" text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
+        <div class="" style="font-size: 20px; text-align: right; padding-right: 0.8rem; padding-top: 2.5rem;">
           <p>8</p>
           <p>Room</p>
         </div>
