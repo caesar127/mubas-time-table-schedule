a524958f95f8a6913b0cbe6df35fc7b2b95ba6f4
Caesar127 <takondwacaesarndawala@gmail.com>
2022-11-04 07:12:03 +0200

    pdf

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -8,11 +8,14 @@
     <table class="">
         <thead class=" bg-gray-50">
             <tr>
+              <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Date</th> 
+              <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Time</th>
+              <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Hours</th>
               <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Module Code</th>
               <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Module Name</th>
               <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Class</th>
-              <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Room</th>
-              <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Time</th> 
+              <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Nos</th>
+              <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Room</th> 
               <th scope="col" class="text-sm font-medium text-gray-900 " style="border-style: solid;">Examiner</th>
             </tr>
         </thead class="">
@@ -20,12 +23,16 @@
             @unless (count($timetable) == 0)
                 @foreach ($timetable as $exam)
                 <tr>
+                  <td class=" whitespace-nowrap text-sm font-medium text-gray-900" style="border-style: solid;">{{$exam->date}}</td>
+                  <td class=" whitespace-nowrap text-sm font-medium text-gray-900" style="border-style: solid;">{{$exam->time}}</td>
+                  <td class=" whitespace-nowrap text-sm font-medium text-gray-900" style="border-style: solid;">3</td>
                     <td class=" whitespace-nowrap text-sm font-medium text-gray-900" style="border-style: solid;">{{$exam->module}}</td>
                     <?php $module =DB::table('module')->select('module_name')->where('module_code',$exam->module)->value('module_name');?>
                     <td class=" whitespace-nowrap text-sm font-medium text-gray-900" style="border-style: solid; border-spacing: 0px;" >{{$module}}</td>
                     <td class=" whitespace-nowrap text-sm font-medium text-gray-900" style="border-style: solid;">{{$exam->class}}</td>
+                    <?php $class =DB::table('class')->select('students')->where('class_code',$exam->class)->value('students');?>
+                    <td class=" whitespace-nowrap text-sm font-medium text-gray-900" style="border-style: solid;">{{$class}}</td>
                     <td class=" whitespace-nowrap text-sm font-medium text-gray-900" style="border-style: solid;">{{$exam->room}}</td>
-                    <td class=" whitespace-nowrap text-sm font-medium text-gray-900" style="border-style: solid;">{{$exam->time}}</td>
                     
                     <?php 
                     $lec =DB::table('module')->select('lecturer')->where('module_code',$exam->module)->value('lecturer');
